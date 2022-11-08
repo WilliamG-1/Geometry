b80123740590f0045cad33172082378ce1e22db1
William <williamgomez32375@gmaill.com>
2022-11-08 11:20:26 -0800

    Transformations now applied to model matrix directly

--- transformations.cpp
+++ transformations.cpp
@@ -12,8 +12,16 @@ void Transformations::scale(glm::mat4& model, Shape2D& shape, float width, float
     model = glm::scale(model, glm::vec3(scalar, scalar, 0.0f)) * glm::translate(glm::mat4(1.0f), glm::vec3(-(shape.get_x() + width/2), -(shape.get_y() + width/2), 0.0f));
 }
 
-void Transformations::rotate3D(glm::mat4& model, Shape2D& shape, float width, float degrees, glm::vec3 axes)
+
+void Transformations::translate3D(glm::mat4& model, float xTranslate, float yTranslate, float zTranslate)
 {
-   // model = glm::translate(glm::mat4(1.0f), glm::vec3(shape.get_x() + width/2, shape.get_y() + width/2, 0.0f));
-    model = glm::rotate(glm::mat4(1.0f), glm::radians(degrees), axes);// * glm::translate(glm::mat4(1.0f), glm::vec3(-shape.get_x() - width/2, -shape.get_y() - width/2, 0.0f));
+    model = glm::translate(model, glm::vec3(xTranslate, yTranslate, zTranslate));
+}
+
+void Transformations::rotate3D(glm::mat4& model, float xRotation, float yRotation, float zRotation, glm::vec3 axes)
+{
+   
+    model = glm::rotate(model, glm::radians(xRotation), glm::vec3(axes.x, 0.0f, 0.0f)) *  
+            glm::rotate(glm::mat4(1.0f), glm::radians(yRotation), glm::vec3(0.0f, axes.y, 0.0f)) *
+            glm::rotate(glm::mat4(1.0f), glm::radians(zRotation), glm::vec3(0.0f, 0.0f, axes.z));
 }
--- transformations.h
+++ transformations.h
@@ -7,7 +7,9 @@ class Transformations
 public:
     static void translate(glm::mat4& model, glm::vec3(position));
     static void rotate2D(glm::mat4& model, Shape2D& shape, float width, float degrees);
-    static void rotate3D(glm::mat4& model, Shape2D& shape, float width, float degrees, glm::vec3 axes);
+
+    static void translate3D(glm::mat4& model, float xTranslate, float yTranslate, float zTranslate);
+    static void rotate3D(glm::mat4& model, float xRotations, float yRotation, float zRotation, glm::vec3 axes);
     static void scale(glm::mat4& model, Shape2D& shape, float width, float scalar);
 private:
 };
