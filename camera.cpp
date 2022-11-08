9ee13094ccdae0bc3e8afa7a2ee69de34e70f8a2
William <williamgomez32375@gmaill.com>
2022-11-08 11:17:26 -0800

    - Created Movable camera - View matrix now uses glm::lookAt for orientation

--- camera.cpp
+++ camera.cpp
@@ -1,5 +1,40 @@
 #include "camera.h"
 
+// Perspective Camera
+PerspectiveCamera::PerspectiveCamera(float fov, float screen_width, float screen_height, float near, float far)
+    :
+    projection(1.0f), view(1.0f), projectionView(1.0f)
+{
+    position = glm::vec3(0.0f, 0.0f, 9.0f);
+    focus = glm::vec3(0.0f, 0.0f, 0.0f);
+    direction = glm::normalize(position - focus);
+
+    glm::vec3 u_p(0.0f, 1.0f, 0.0f); // Vector specifying up direction in our world
+    right = glm::normalize(glm::cross(direction, u_p));
+
+    up = glm::normalize(glm::cross(up, direction));
+
+    projection = glm::perspective(glm::radians(fov), screen_width/screen_height, near, far);
+    view = glm::lookAt(position, focus, glm::vec3(0.0f, 1.0f, 0.0f));
+
+    recalculate();
+}
+
+void PerspectiveCamera::moveView(glm::vec3 position)
+{
+    this->position = position;
+    focus = glm::vec3(position.x, position.y, 0.0f);
+    view = glm::lookAt(position, focus, glm::vec3(0.0f, 1.0f, 0.0f));
+    recalculate();
+}
+void PerspectiveCamera::recalculate()
+{
+    projectionView = projection * view;
+}
+
+
+
+
 // OrthoGrapic Camera
 OrthographicCamera::OrthographicCamera(float screen_width, float screen_height)
     :
@@ -17,25 +52,6 @@ void OrthographicCamera::moveView(glm::vec3 position)
 }
 
 void OrthographicCamera::recalculate()
-{
-    projectionView = projection * view;
-}
-
-// Perspective Camera
-PerspectiveCamera::PerspectiveCamera(float fov, float screen_width, float screen_height, float near, float far)
-    :
-    projection(1.0f), view(1.0f), projectionView(1.0f)
-{
-    projection = glm::perspective(glm::radians(fov), screen_width/screen_height, near, far);
-    recalculate();
-}
-
-void PerspectiveCamera::moveView(glm::vec3 position)
-{
-    view = glm::translate(glm::mat4(1.0f), position);
-    recalculate();
-}
-void PerspectiveCamera::recalculate()
 {
     projectionView = projection * view;
 }
--- camera.h
+++ camera.h
@@ -2,19 +2,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-class OrthographicCamera
-{
-public:
-    OrthographicCamera(float screen_width, float screen_height);
-    void moveView(glm::vec3 position);
-    void recalculate();
-
-    glm::mat4 get_projection_view_matrix() {return projectionView;}
-private:
-    glm::mat4 projectionView;
-    glm::mat4 view;
-    glm::mat4 projection;
-};
 
 class PerspectiveCamera
 {
@@ -29,4 +16,24 @@ private:
     glm::mat4 view;
     glm::mat4 projectionView;
 
+    glm::vec3 position;     // Position of our camera
+    glm::vec3 focus;        // Position that our camera is lookint at
+    glm::vec3 direction;    // Direction vector (Note that it points towards our camera, not the focus)
+    glm::vec3 right;
+    glm::vec3 up;
+};
+
+
+class OrthographicCamera
+{
+public:
+    OrthographicCamera(float screen_width, float screen_height);
+    void moveView(glm::vec3 position);
+    void recalculate();
+
+    glm::mat4 get_projection_view_matrix() {return projectionView;}
+private:
+    glm::mat4 projectionView;
+    glm::mat4 view;
+    glm::mat4 projection;
 };
