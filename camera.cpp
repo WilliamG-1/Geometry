2bbb40be5dd704bb810ef790fbda03fb7aa6c095
William <williamgomez32375@gmaill.com>
2022-11-08 13:57:47 -0800

    - Added Freeflying camera using euler angles - Tweaked movement system

--- camera.cpp
+++ camera.cpp
@@ -1,5 +1,6 @@
 #include "camera.h"
-
+#include <iostream>
+#include <math.h>
 // Perspective Camera
 PerspectiveCamera::PerspectiveCamera(float fov, float screen_width, float screen_height, float near, float far)
     :
@@ -7,30 +8,89 @@ PerspectiveCamera::PerspectiveCamera(float fov, float screen_width, float screen
 {
     position = glm::vec3(0.0f, 0.0f, 9.0f);
     focus = glm::vec3(0.0f, 0.0f, 0.0f);
-    direction = glm::normalize(position - focus);
 
+    euler_angle(90, 90);
+
+    direction = glm::normalize(position - focus);
     glm::vec3 u_p(0.0f, 1.0f, 0.0f); // Vector specifying up direction in our world
     right = glm::normalize(glm::cross(direction, u_p));
 
-    up = glm::normalize(glm::cross(up, direction));
-
     projection = glm::perspective(glm::radians(fov), screen_width/screen_height, near, far);
-    view = glm::lookAt(position, focus, glm::vec3(0.0f, 1.0f, 0.0f));
+    view = glm::lookAt(position, front, glm::vec3(0.0f, 1.0f, 0.0f));
 
+    recalculate();
+}
+
+void PerspectiveCamera::euler_angle(float phi, float theta)
+{
+    float p = 1;
+    // Assume lenght of vector is 1 (normalized) so rho (p) is going to be 1 
+    // General equations  Here, open gl uses right hand system, but the y is up and the z is forwards
+    // x = p * sin(phi) * cos(theta)
+    // z = p * sin(phi) * sin(theta)
+    // y = p * cos(phi)
+    // For camera looking forwards, phi = pi/2 | theta = pi/2
+    
+    front.x = p * sin(glm::radians(phi)) * cos(glm::radians(theta));
+    front.z = p * sin(glm::radians(phi)) * sin(glm::radians(theta));
+    front.y = p * cos(glm::radians(phi));
+    
+    front = glm::normalize(front);
+
+    // Negate values because the negative axis is "Forwards" in our coordinate system
+    front.x *= -1;
+    front.y *= -1;
+    front.z *= -1;
+    //std::printf("Direction: (%.2f, %.2f, %.2f)\n", front.x, front.y, front.z);
+    update_basis();
+
+    view = glm::lookAt(position, position + front, glm::vec3(0.0f, 1.0f, 0.0f));
+    recalculate();
+}
+
+void PerspectiveCamera::move(CameraDirection direction, float dt)
+{
+    switch (direction)
+    {
+    case CameraDirection::FORWARD:
+        position += front * dt * velocity;
+        break;
+    case CameraDirection::BACKWARD: // Backward
+        position -= front * dt * velocity;
+        break;
+    case CameraDirection::LEFT: // Backward
+        position -= right * dt * velocity;
+        break;
+    case CameraDirection::RIGHT: // Backward
+        position += right * dt * velocity;
+        break;
+    }
+    view = glm::lookAt(position, position + front, glm::vec3(0.0f, 1.0f, 0.0f));
     recalculate();
 }
 
 void PerspectiveCamera::moveView(glm::vec3 position)
 {
-    this->position = position;
-    focus = glm::vec3(position.x, position.y, 0.0f);
-    view = glm::lookAt(position, focus, glm::vec3(0.0f, 1.0f, 0.0f));
+    this->position = front + position;
+    view = glm::lookAt(position, position + front, glm::vec3(0.0f, 1.0f, 0.0f));
     recalculate();
 }
 void PerspectiveCamera::recalculate()
 {
     projectionView = projection * view;
 }
+
+void PerspectiveCamera::update_basis()
+{
+    right = glm::normalize(glm::cross(front, glm::vec3(0.0f, 1.0f, 0.0f)));
+   // std::printf("Right: (%.2f, %.2f, %.2f)\n", right.x, right.y, right.z);
+}
+
+
+
+
+
+
 
 
 
--- camera.h
+++ camera.h
@@ -2,20 +2,39 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+enum class CameraDirection
+{
+    FORWARD = 0,
+    BACKWARD = 1,
+    LEFT = 2,
+    RIGHT = 3,
+    UP = 4,
+    DOWN = 5
+};
 
 class PerspectiveCamera
 {
 public:
     PerspectiveCamera(float fov, float screen_width, float screen_height, float near, float far);
+
+    void euler_angle(float phi, float theta);
+    void move(CameraDirection direction, float dt);
+    void update_basis();
     void moveView(glm::vec3 position);
     void recalculate();
 
+    glm::vec3 get_position() const {return position;}
+    glm::vec3 get_front() const {return front;}
+    glm::vec3 get_focus() const {return position + front;}
     glm::mat4 get_projection_view_matrix() {return projectionView;}
 private:
+    float velocity = 30;
+    
     glm::mat4 projection;
     glm::mat4 view;
     glm::mat4 projectionView;
 
+    glm::vec3 front;
     glm::vec3 position;     // Position of our camera
     glm::vec3 focus;        // Position that our camera is lookint at
     glm::vec3 direction;    // Direction vector (Note that it points towards our camera, not the focus)
