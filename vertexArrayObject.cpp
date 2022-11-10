06a91bb9d0cf0ba6b1cfa69135cb704a6b605f21
William <williamgomez32375@gmaill.com>
2022-11-09 21:59:07 -0800

    - Created vertexattribptr function

--- vertexArrayObject.cpp
+++ vertexArrayObject.cpp
@@ -7,7 +7,7 @@ VertexArrayObject::VertexArrayObject()
     glBindVertexArray(vao);
 }
 
-void VertexArrayObject::init_VBO(float* data, int number_of_elements, unsigned int location, unsigned int count, unsigned int stride, int offset)
+void VertexArrayObject::init_VBO(float* data, int number_of_elements, unsigned int location, unsigned int count_per_vertices, unsigned int stride, int offset)
 {
     
     //glGenBuffers(1, &ebo);
@@ -17,8 +17,11 @@ void VertexArrayObject::init_VBO(float* data, int number_of_elements, unsigned i
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * number_of_elements, data, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, count_per_vertices, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)(offset * sizeof(float)));
     glEnableVertexAttribArray(location);
+
+    // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)(3 * sizeof(float)));
+    // glEnableVertexAttribArray(1);
 
     //glBindVertexArray(0);
     //glDeleteBuffers(1, &vbo);
@@ -32,6 +35,17 @@ void VertexArrayObject::init_EBO(unsigned int* data, int number_of_elements)
     
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * number_of_elements, data, GL_STATIC_DRAW);
+
+}
+
+void VertexArrayObject::set_vertex_attrib_pointer(float* data, int number_of_elements, unsigned int location, unsigned int count, unsigned int stride, int offset)
+{
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * number_of_elements, data, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(location, count, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)(offset * sizeof(float)));
+    glEnableVertexAttribArray(location);
 
 }
 
