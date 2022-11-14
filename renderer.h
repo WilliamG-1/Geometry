f15af2b9928a772b0a88ed67b4f3fa30b995c978
William <williamgomez32375@gmaill.com>
2022-11-13 17:36:47 -0800

    Added material to rendering argument

--- renderer.h
+++ renderer.h
@@ -5,12 +5,22 @@
 #include "shader.h"
 #include "shape2D.h"
 #include "shape3D.h"
+
+struct Material{
+    glm::vec3 ambient;
+    glm::vec3 diffusion;
+    glm::vec3 specular;
+
+    float shininess;
+};
+
 class Renderer
 {
 public:
     Renderer();
 
     void draw2D(Shader& shader, Shape2D& shape);
+    void draw3D(Shader& shader, Shape3D& shape, Material material);
     void draw3D(Shader& shader, Shape3D& shape);
 private:
 
