319149fcb19d0646d5cc32c2009d6aa0a8a92963
William <williamgomez32375@gmaill.com>
2022-11-16 20:49:18 -0800

    Added Assimp Library

---
+++ mesh.h
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+
+#include "shader.h"
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+};  
+class Mesh {
+    public:
+        // mesh data
+        std::vector<Vertex>       vertices;
+        std::vector<unsigned int> indices;
+        std::vector<Texture>      textures;
+
+        Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+        void Draw(Shader &shader);
+    private:
+        //  render data
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh();
+};  
